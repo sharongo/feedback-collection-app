2869f803110f81f8eb865938bd066bde6a8180c4
Sharon Gold <sharongo113@gmail.com>
2019-09-17 17:30:10 +0300

    add auth

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+
+mongoose
+    .connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful!'));
+
 
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`)
+});
